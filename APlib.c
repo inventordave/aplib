f701c7b27e741aff6864bff4f14bab5db75d4b60
Inventor Dave <102000247+inventordave2@users.noreply.github.com>
2024-08-03 02:20:44 +0100

    quick backup

--- APlib.c
+++ APlib.c
@@ -1353,34 +1353,7 @@ char** getaplibsymbols(){
 	
 	char** symbols = (char**)malloc( sizeof( char* )*655536 ); // this implies up to 64k symbols in list.
 	
-	#define UNKNOWN 0
-	#define OP_AND 1
-	#define OP_OR 2
-	#define OP_XOR 3
-	#define OP_NOT 4
-	#define OP_NAND 5
-	
-	#define OP_PLUS 17
-	#define OP_MINUS 18
-	#define OP_STAR 19
-	#define OP_FORWARDSLASH 20
-	
-	#define OP_EQUALS 21
-	#define OP_LTORISEQUALTO 22
-	#define OP_PLUSEQUALS 23
-	#define OP_MINUSEQUALS 24
-	#define OP_ISEQUALTO 25
-	#define OP_NOTEQUALTO 26
-	#define OP_GTORISEQUALTO 27
-	#define OP_GT 28
-	#define OP_LT 29
-	
-	#define OP_CARAT 30
-	#define OP_RATIO 31
-	#define OP_RATIO_ASSIGN 32
-	#define OP_RATIO_ISEQUALTO 33
-	
-	
+	symbols[DELIMITERS] = getstring( "([{}])" );
 	symbols[UNKOWN] = getstring("UNKNOWN");
 	symbols[OP_AND] = getstring("AND");
 	symbols[OP_OR] = getstring("OR");
@@ -1395,18 +1368,6 @@ char** getaplibsymbols(){
 	//symbols[OP_] = getstring("");	
 	symbols[OP_GT] = getstring( ">" );
 	symbols[OP_LT] = getstring("<");
-
-	#define OP_INCREMENT 34
-	#define OP_DECREMENT 35
-	#define OP_ISDEFINEDAS 36
-	#define OP_CONDITIONAL 37
-	#define OP_BITWISE_AND 38
-	#define OP_BITWISE_NOT 39
-	#define OP_BITWISE_OR 40
-	#define OP_PERIOD 41
-	#define OP_LOGICAL_AND 42
-	#define OP_LOGICAL_OR 43
-	#define OP_LOGICAL_ISNOT 44
 	
 	
 	
--- APlib.h
+++ APlib.h
@@ -211,6 +211,50 @@ int str2int(char *input);
 char * int2str(int v);
 
 char** getaplibsymbols();
+// 	getaplibsymbols() DEFS. 
+
+//char* delimiters = strdup( "([{}])" );
+#define DELIMITERS 999
+
+#define UNKNOWN 0
+#define OP_AND 1
+#define OP_OR 2
+#define OP_XOR 3
+#define OP_NOT 4
+#define OP_NAND 5
+
+#define OP_PLUS 17
+#define OP_MINUS 18
+#define OP_STAR 19
+#define OP_FORWARDSLASH 20
+
+#define OP_EQUALS 21
+#define OP_LTORISEQUALTO 22
+#define OP_PLUSEQUALS 23
+#define OP_MINUSEQUALS 24
+#define OP_ISEQUALTO 25
+#define OP_NOTEQUALTO 26
+#define OP_GTORISEQUALTO 27
+#define OP_GT 28
+#define OP_LT 29	
+
+#define OP_CARAT 30
+#define OP_RATIO 31
+#define OP_RATIO_ASSIGN 32
+#define OP_RATIO_ISEQUALTO 33
+
+#define OP_INCREMENT 34
+#define OP_DECREMENT 35
+#define OP_ISDEFINEDAS 36
+#define OP_CONDITIONAL 37
+#define OP_BITWISE_AND 38
+#define OP_BITWISE_NOT 39
+#define OP_BITWISE_OR 40
+#define OP_PERIOD 41
+#define OP_LOGICAL_AND 42
+#define OP_LOGICAL_OR 43
+#define OP_LOGICAL_ISNOT 44
+
 
 #endif
 
