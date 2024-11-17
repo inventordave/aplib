d5258fb0fcca1501d2ac7f87f08c1148f3da31b3
Dave <digital.artist@hotmail.co.uk>
2024-11-17 09:28:29 +0000

    Update fpcalc.c

--- fpcalc.c
+++ fpcalc.c
@@ -1,33 +1,46 @@
-    #include <stdio.h>
-	#include <stdlib.h>
-	#include <math.h>
-	#include <string.h>
-	#include <float.h>
-	
-	#include "fractionalcalc.tab.h"
-	
-    int yylex();
-    int yyerror(const char*);
-	
-	void printVars();
-	char *convert(double x);
-	char *trim(double x);
-	char *(*function)(); 
-	
-	scint truthy(double);
-	
-	double _FALSE;
-	double _TRUE;
-	double _Null;
-	scint truthyFlag;
-	
-	double vars[256] = { -10000 };
-	char*  evars[256] = { '\0' };
-	
-	scint optionsFlag = 0;
-	
-	char* enum_str;
-	extern double generateExpValue(char *);
-	
-	#define NUM_STRING_LENGTH 325 // max length of fractional (double) number, including decimal point. Obvs is way less than 100.
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <float.h>
+
+#include "fractionalcalc.tab.h"
+
+#include "aplib.h"
+#include "colour.h"
+#include "stringy.h"
+
+// LEX FNC'S. (Will replace these with my 'simplelexer.c' library.
+int yylex();
+int yyerror(const char*);
+
+void printVars();
+
+// These are deprecated functions. They perform digitstring-truncation, which
+// is a feature precision, which 'aplib' will implement instead. 
+char *convert(double x);
+char *trim(double x);
+char *(*function)(); 
+
+
+scint truthy(double);
+
+// These methods of encoding FALSE, TRUE, NULL,
+// will be seperated from any Real- or Integer-valued scalar[0,N]
+// by aplib.
+double _FALSE;
+double _TRUE;
+double _Null;
+scint truthyFlag;
+
+// User-defined Variables & Expression-Variables
+double vars[256] = { -10000 };
+char*  evars[256] = { '\0' };
+
+scint optionsFlag = 0;
+
+char* enum_str;
+extern double generateExpValue(char *);
+
+#define NUM_STRING_LENGTH 325 // max length of fractional (double) number, including decimal point. Obvs is way less than 100.
 
