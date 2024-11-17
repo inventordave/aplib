c58a05d4f2c24b75b06a857ce5a92f6dc5420815
Dave <digital.artist@hotmail.co.uk>
2024-11-17 12:57:50 +0000

    Create memory.c

---
+++ gcollect/memory.c
@@ -0,0 +1,50 @@
+// MEMORY_C
+
+#include "memory.h"
+
+char* strtolower( char* _ )	{
+	
+	L strlen__ = strlen( _ );
+	char* s = (char*)malloc( strlen__ + 1 );
+	char* t = s;
+	char c;
+	while( strlen__-- )	{
+
+		c = *(_++);
+		if( (c>='A') && (c<='Z') )
+		*t++ = c - ('A' - 'a');
+	}
+	
+	*t = 0;
+	
+	return s;
+}
+char* strtoupper( char* _ )	{
+
+	L strlen__ = strlen( _ );
+	char* s = (char*)malloc( strlen__ + 1 );
+	char* t = s;
+	char c;
+	while( strlen__-- )	{
+
+		c = *(_++);
+		if( (c>='a') && (c<='z') )
+		*t++ = c - ('A' - 'a');
+	}
+	
+	*t = 0;
+	
+	return s;
+}
+
+void* davealloc( L size, uint8_t item )	{
+	#include <ctype.h>
+	void* _ = malloc( size );
+	char* t = (char*)_;
+	
+	while( (size--) > 0 )
+		*t++ = item;
+
+	return _;
+}
+
