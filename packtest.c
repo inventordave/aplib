790df3e25a6a2ce8b434a86e65ef8da289c26d66
Dave <digital.artist@hotmail.co.uk>
2024-11-19 08:36:50 +0000

    Update packtest.c

--- packtest.c
+++ packtest.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
-#define L unsigned long long int
+#define LARGE long long int
+#define L LARGE
+#define large L
 
 #define strlen strlen_
 L strlen_(char * str)	{
 	
-	L i = 0;
+	large i = 0;
 	while( str[i++] != '\0' )
 		;
 	return i-1;
@@ -46,8 +48,8 @@ char* unpack( char* _ ) {
 	
 	L strlen__ = strlen( _ );
 
-	int BITMASK_LOWER = 15;
-	int BITMASK_UPPER = 255-15; //240, which is 11110000 (128+64+32+16)
+	int BITMASK_LOWER = 15;		// 00001111 (8+4+2+1)
+	int BITMASK_UPPER = 255-15; // 11110000 (128+64+32+16)
 	char t;
 	
 	L i, j;
@@ -73,6 +75,138 @@ char* unpack( char* _ ) {
 		_U[j] = '\0';
 
 	return _U;
+}
+
+/**
+
+a-z0-9		+ - * / = _ . , ! ? ~ % []{}() @ # $ \ and an nbsp whitespacecode.
+
+(definitely need 5 bits, to encode 26 alphabetical chars a-z and the escape code \ 
+which would leave 5 codes to fit in [0, 31]
+If I just used a-z and \ then everything else, 0-9, null, etc would need to fit between [\a, \z], which gives
+me 26 alphabet codes 'a' to 'z', the escape delimeter '\', 26 meta codes '\a' to '\z' and meta code '\\'
+
+which is 54 unique codes (a-z and \ makes 27. Escaping each \x gives 27*2, which is 54 codes/metacodes).
+
+27 primitives, a to z and '\', gives 5 spare primitive encodings, as the range of 5 bits is 32 [min=0, max=31]
+
+
+
+
+
+
+a code for capitalisation, or an escape sequence to represent capitalisation, such as \c4 might indicate that the next 4 alphabet...
+chars be capitalised.
+
+->	\c0 might indicate a proceeding sequence is chomsky-language code
+->	\ca might indicate capitalisation. note that the escape sequence is \ca, so capitalise a proceeding 'a', it would be \caa
+	and therefore to capitalise 'x' would be \cax
+->	\cn might indicate an entity name is proceeding.
+
+a code for repetition. \l, eg:
+
+\la1024y might repeat the letter 'y' 1024 times.
+\ln1024.0 might repeat the didigt '0' 1024 times.
+
+\l (a|n) (decimal sequence indicating amount of repetitions) ( (.[numerical_digit]) | alphabetical_char ) // note the leading '.' for numeric
+
+
+\d		delimeter, \d1 := (	\d2 := [	\d3 := {
+		\d1a = (, \d1b = ), \d2a = [, \d2b = ], \d3a = {, \d3b = }
+\0		nul
+Just one escape and an operand to indicate base number system, eg: \b8 would be octal, base 8.
+\bb \bl might mean booleanbitwise and booleanlogical
+\a and
+\| or
+\! not
+\t
+\n
+\r
+\e
+
+extra candidate escape sequences
+\%[a-z0-9_@ ... etc ...]
+
+
+{base,exponent}
+{key,value}
+{author}
+{utc}
+
+*/
+
+char encode( char c )	{
+
+	#define ESCAPECODE '\'
+	#define CODE1 '.'
+	#define CODE2 '\0'
+	#define CODE3 '%'
+	#define CODE4 '*'
+	#define CODE5 '+'
+
+	char _;
+	
+	if( (c>='a')&&(c<='z') )
+		; // alphabet char
+
+	if( c=='\' )
+		; // the escape delimeter
+
+	switch( c )	{
+
+		break;
+		case CODE1:
+
+			break;
+			
+		break;
+		case CODE2:
+
+			break;
+			
+		break;
+		case CODE3:
+
+			break;
+			
+		break;
+		case CODE4:
+
+			break;
+
+		break;
+		case CODE5:
+			
+			break;
+		
+		break;
+		default:
+
+			break;
+	}
+
+	// ... algorithm goes here
+
+	return _;
+}
+
+pack5bits( char* _ )	{
+
+	int i;
+	char* packed;
+
+	int strlen__ = strlen( _ );
+	char u = 0;
+	char p = 0;
+	char t = 0;
+	for( i=0; i<strlen__; i++ )	{
+		u = _[i];
+
+		t = encode( u );
+
+		
+	}
+	
 }
 
 // assumes an unpacked string of 1 char per decimal digit.
