da052a781c1bffc4b6c2101fbf424e0ba0c20bfa
Dave <digital.artist@hotmail.co.uk>
2024-11-26 07:33:54 +0000

    Update lexer.h

--- lexer/lexer.h
+++ lexer/lexer.h
@@ -3,6 +3,9 @@
 #define DAVELIB_SIMPLE_LEXER_H
 
 // GLOBALS
+
+
+
 typedef struct LexInstance	{
 
 	int TOK_TYPE;
@@ -24,7 +27,37 @@ typedef struct LexInstance	{
 
 } LexInstance;
 
+typedef struct ParseInstance	{
+
+	struct LexInstance* lexer;
+	int (*parse)( struct ParseInstance* self );
+
+	char* CFG; // filename of .cfg production rules file.
+	int numPRs; // number of production rules.
+
+	struct CSTNode* Root;
+
+	
+} ParseInstance;
+
+
+typedef struct CSTNode	{
+
+	void** descendants; // [ typeof( struct CSTNode* ) ]
+	void* ancestor; 	// typeof( struct CSTNode* )
+	char* nodeName;
+	char** T_NT;
+	unsigned numEntries;
+
+	int (*extend)( void* self );
+
+}	CSTNode;
+
+
 // FUNCTIONS
+struct CSTNode* initNode( char* nodeName, int numEntries );
+int extend( void* _ );
+
 char*** initRuleSetArray( int numRules );
 char*** initTokenResultsArray( int assumpt );
 
@@ -33,6 +66,9 @@ char* patternMatch( char* str, struct LexInstance* );
 
 int lex( struct LexInstance* );
 void push( char* token_type, char* literal, struct LexInstance* );
+
+// int Parse( struct ParseInstance* );
+// void pushFromParser( struct CSTNode* nodeName, char* literal );
 
 #endif // DAVELIB_SIMPLE_LEXER_H
 
